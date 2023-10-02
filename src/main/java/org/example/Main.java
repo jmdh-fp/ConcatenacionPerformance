904355e55d5fb4e52328a0d3f2e05ca664674662
jm-upmd <43405929+jm-upmd@users.noreply.github.com>
2023-10-02 21:13:39 +0200

    Commit

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,7 +1,34 @@
 package org.example;
 
 public class Main {
+    static final int CUENTA = 10_000_000;
+    static String trozoTexto = "El patio de mi casa es particular, llueve y se moja como los demás";
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        long tiempo = System.currentTimeMillis();
+       /* concatenaString();
+        System.out.println("Tiempo concatena String: " + (System.currentTimeMillis() - tiempo) / 1000);
+        tiempo = System.currentTimeMillis();*/
+        concatenaStringBuilder();
+        System.out.println("Tiempo concatena String: " + (System.currentTimeMillis() - tiempo) / 1000);
+    }
+
+    private static void concatenaString(){
+        String textoTotal="";
+
+        for (int i = 1; i <=CUENTA ; i++) {
+            textoTotal += trozoTexto;
+        }
+        System.out.println("Tamaño del texto total: " + textoTotal.length());
+    }
+
+    private static void concatenaStringBuilder(){
+        StringBuilder textoTotal = new StringBuilder();
+        for (int i = 1; i <= CUENTA; i++) {
+            textoTotal.append(trozoTexto);
+        }
+        String totalTexto = textoTotal.toString();
+        System.out.println("Tamaño del texto total: " + totalTexto.length());
+
+
     }
 }
